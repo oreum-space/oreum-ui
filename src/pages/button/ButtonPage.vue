03f681c2be12fa07d04ed1df21ac232254996c54
dbih91 <179danil@gmail.com>
2024-10-04 17:21:27 +0300

    ButtonPage.vue sizes display added

--- src/pages/button/ButtonPage.vue
+++ src/pages/button/ButtonPage.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
-import { OButton, OButtonVariants } from '@lib/oreum-ui'
+import { OButton, OButtonSizes, OButtonVariants } from '@lib/oreum-ui'
+
+function excludeDefault <T>(_enum: T): T {
+  return Object.fromEntries(Object.entries(_enum).filter(([key]) => key !== 'default')) as T
+}
+
+const sizes = excludeDefault(OButtonSizes)
+const variants = excludeDefault(OButtonVariants)
 </script>
 
 <template>
-  <section class="">
-    <div class="doc-card">
+  <section>
+    <div
+      v-for="size in sizes"
+      :key="size"
+      class="doc-card"
+    >
+      {{ size }}
       <OButton
-        v-for="variant in OButtonVariants"
+        v-for="variant in variants"
         :key="variant"
+        :size="size"
         class="o_capitalize"
         :variant="variant"
         :label="variant"
-        />
+      />
     </div>
   </section>
 </template>
